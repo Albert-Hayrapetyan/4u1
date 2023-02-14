89dff9a0e959a1b23415400adc1a29621bfc8060
Albert-Hayrapetyan <79860506+Albert-Hayrapetyan@users.noreply.github.com>
2023-02-15 00:14:16 +0400

    Delete Valentine3 directory

--- Valentine3/main.js
+++
@@ -1,9 +0,0 @@
-$( document ).ready(() => {
-    $(".envelope-wrapper .heart").click(() => {
-      $('.envelope-wrapper').addClass('flap')
-  });
-
-  $(".envelope-wrapper .close-icon").click(() => {
-      $('.envelope-wrapper').removeClass('flap')
-  });
-});
